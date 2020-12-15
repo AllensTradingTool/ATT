2aada1cbb7d9962d7358d2d84805adfa309d40d1
AllensTradingTool <75588537+AllensTradingTool@users.noreply.github.com>
2020-12-15 10:27:38 -0800

    Delete module_installer.bat

--- module_installer.bat
+++
@@ -1,4 +0,0 @@
-call npm install fs alphavantage chalk jsdom axios express body-parser --save
-call ECHO Finished download nessessary node modules..... Will now start server 
-call timeout /t 16
-call startATT.bat
